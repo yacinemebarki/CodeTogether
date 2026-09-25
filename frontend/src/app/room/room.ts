6c162a5a6b36ba1723df89b678c6b99bc03a8065
mebarkiyacine <mebarkiyacine822@gmail.com>
2026-09-25 16:04:06 +0100

    start building room content

--- frontend/src/app/room/room.ts
+++ frontend/src/app/room/room.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-room',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './room.html',
   styleUrl: './room.css',
 })
 export class Room {
   time: number = 0;
   code = 'abcdeft';
+  isClicked = false;
+  selectedLanguage = '';
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+
+  copie(){
+    navigator.clipboard.writeText(this.code);
+    this.isClicked = true;
+
+    setTimeout(() => {
+        this.isClicked = false;
+        this.cdr.markForCheck();
+    }, 300);
+  }
+
+  ShowProblem(){
+
+  }
+
+  ShowList(){
+
+  }
+
+  ToChat(){
+
+  }
+
+  ToMember(){
+
+  }
 }
